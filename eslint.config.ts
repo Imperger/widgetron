2e0eb6032b39c917bf8cecfe0f203cb2cdc1e788
Imperger <mr.imperger@hotmail.com>
2025-03-13 07:49:49 +0000

    chore: turn off `import/no-unresolved` because failed to resolve aliases

--- eslint.config.ts
+++ eslint.config.ts
@@ -18,7 +18,7 @@ export default defineConfigWithVueTs(
     },
     rules: {
       '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
-      'import/no-unresolved': 'error',
+      'import/no-unresolved': 'off',
       'import/order': [
         'error',
         {
